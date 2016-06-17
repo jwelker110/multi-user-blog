1cb586c6bb3231775be3296807120476620b75f7
Jon Welker <JWelker110@gmail.com>
2016-06-17 05:36:16 -0500

    set template dir based on cwd

--- app/helpers/templates.py
+++ app/helpers/templates.py
@@ -2,7 +2,7 @@ import os
 import jinja2
 import webapp2
 
-template_dir = os.path.join(os.path.dirname(__file__), '../../templates/')
+template_dir = os.path.join(os.getcwd(), 'templates/')
 J = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir),
     extensions=['jinja2.ext.autoescape'],
