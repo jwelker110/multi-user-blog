2c988cbd330ecf624650345e43c0e246efede0c2
Jon Welker <JWelker110@gmail.com>
2016-06-19 13:11:29 -0500

    Remove email from user model

--- app/models/user.py
+++ app/models/user.py
@@ -5,5 +5,4 @@ class User(ndb.Model):
     username = ndb.StringProperty(required=True)
     username_lower = ndb.StringProperty(required=True)
     password = ndb.StringProperty(required=True)
-    email = ndb.StringProperty()
     joined = ndb.DateTimeProperty(auto_now_add=True)
