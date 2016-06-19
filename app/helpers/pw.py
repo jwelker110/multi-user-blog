bb6e778c0ac1e11eb3c636304f66cacf88125eb6
Jon Welker <JWelker110@gmail.com>
2016-06-19 06:55:05 -0500

    Update to no longer use the non-python random since GAE doesn't allow it

--- app/helpers/pw.py
+++ app/helpers/pw.py
@@ -1,13 +1,13 @@
+import random
 import string
 
 from secret import SECRET
 from lib.Crypto.Hash import SHA512
-from lib.Crypto.Random import random
 
 
 def gen_hash(pw, salt=None):
     if salt is None:
-        salt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in xrange(32))
+        salt = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in xrange(32))
     return SHA512.new(salt + SECRET + pw).hexdigest() + '|' + salt
 
 
