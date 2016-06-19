66b74d6deb536df46f80d990db89cbb9ea020055
Jon Welker <JWelker110@gmail.com>
2016-06-19 16:28:30 -0500

    Remove file upload it is not part of project spec

---
+++ app/forms/post.py
@@ -0,0 +1,16 @@
+from lib.wtforms.form import Form
+from lib.wtforms.fields.core import StringField
+from lib.wtforms.fields.simple import TextAreaField, HiddenField
+from lib.wtforms import validators
+
+
+class PostForm(Form):
+    title = StringField('Title',
+                        [validators.length(max=50, message='Titles must not exceed 50 chars.'),
+                         validators.required(message='Please enter a title.')])
+    subject = StringField('Subject',
+                          [validators.length(max=100, message='Subject must not exceed 100 chars.')])
+    content = TextAreaField('Content',
+                            [validators.length(max=1800, message='Content must not exceed 1,800 chars.'),
+                             validators.required(message='Please enter some content for the post.')])
+    csrf_token = HiddenField()
