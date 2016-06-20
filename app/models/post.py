058996ec6ce373ebb62122fac538e94650ce58ef
Jon Welker <JWelker110@gmail.com>
2016-06-19 19:52:33 -0500

    Add lowercase title field

--- app/models/post.py
+++ app/models/post.py
@@ -3,6 +3,7 @@ from google.appengine.ext import ndb
 
 class Post(ndb.Model):
     title = ndb.StringProperty(required=True)
+    title_lower = ndb.StringProperty(required=True)
     author = ndb.StringProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
     subject = ndb.StringProperty()
