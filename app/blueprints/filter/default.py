0ffaa9291791a527241073b24755792adaa22faf
Jon Welker <JWelker110@gmail.com>
2016-06-21 04:47:19 -0500

    Create handler to retrieve most recent posts

--- app/blueprints/filter/default.py
+++ app/blueprints/filter/default.py
@@ -1,5 +1,26 @@
-from app.helpers import Helper
+from app.helpers import Helper, flash
+from app.models import Post
+
+temp = 'index.html'
 
 
 class DefaultHandler(Helper):
-    pass
+    def r(self, posts=None, offset=0, limit=0, template=temp, **kw):
+        self.render(template, posts=posts, offset=offset, limit=limit, **kw)
+
+    def get(self):
+        flashes = []
+        limit = 20
+        offset = self.request.get_range('offset', min_value=0, max_value=100000, default=0)
+
+        # let's get the most recent posts
+        posts = Post.query().order(-Post.created).fetch(limit, offset=offset)
+
+        if len(posts) is 0 and offset > 0:
+            flashes = flash('No more posts available')
+            offset -= limit
+            posts = Post.query().order(-Post.created).fetch(limit, offset=offset)
+
+        self.r(posts, offset, limit, flashes=flashes)
+
+
