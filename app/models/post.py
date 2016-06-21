451e479b8f9d9b81b76329e6714565fab7e5e178
Jon Welker <JWelker110@gmail.com>
2016-06-21 05:52:13 -0500

    Add lowercase author name

--- app/models/post.py
+++ app/models/post.py
@@ -5,6 +5,7 @@ class Post(ndb.Model):
     title = ndb.StringProperty(required=True)
     title_lower = ndb.StringProperty(required=True)
     author = ndb.StringProperty(required=True)
+    author_lower = ndb.StringProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
     subject = ndb.StringProperty()
     content = ndb.TextProperty(required=True)
