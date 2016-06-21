a16e44188d7d836e0f3079cebcf544a936705e8a
Jon Welker <JWelker110@gmail.com>
2016-06-21 04:44:46 -0500

    Add filter to shorten lengthy text

--- app/helpers/filters.py
+++ app/helpers/filters.py
@@ -3,3 +3,7 @@ from datetime import datetime
 
 def datetimefilter(value, f='%a %b %d, %Y'):
     return datetime.strftime(value, f)
+
+
+def shortenfilter(value, size=47):
+    return value if len(value) < 48 else value[:size] + '...'
