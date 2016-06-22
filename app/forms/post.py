38d543dddec708a03c90e93243bbc197983a3112
Jon Welker <JWelker110@gmail.com>
2016-06-22 12:15:21 -0500

    Add form to edit the post

--- app/forms/post.py
+++ app/forms/post.py
@@ -14,3 +14,16 @@ class PostForm(Form):
                             [validators.length(max=1800, message='Content must not exceed 1,800 chars.'),
                              validators.required(message='Please enter some content for the post.')])
     csrf_token = HiddenField()
+
+
+class PostEditForm(Form):
+    title = StringField('Title',
+                        [validators.length(max=50, message='Titles must not exceed 50 chars.'),
+                         validators.required(message='Please enter a title.')])
+    subject = StringField('Subject',
+                          [validators.length(max=100, message='Subject must not exceed 100 chars.')])
+    content = TextAreaField('Content',
+                            [validators.length(max=1800, message='Content must not exceed 1,800 chars.'),
+                             validators.required(message='Please enter some content for the post.')])
+    key = HiddenField()
+    csrf_token = HiddenField()
