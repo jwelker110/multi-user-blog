33c0ba1b0efe95c661848d99deb04a3df19ff319
Jon Welker <JWelker110@gmail.com>
2016-06-24 05:06:14 -0500

    Users may comment on posts

--- app/blueprints/comment/create.py
+++ app/blueprints/comment/create.py
@@ -1,3 +1,41 @@
-from app import J
+from google.appengine.ext.ndb import Key
+
+from app.helpers import Helper
+from app.forms import CommentForm
+from app.models import Comment
 
 
+class CommentCreateHandler(Helper):
+    def post(self):
+        # make sure our user is here
+        user = self.validate_user()
+        if user is None:
+            self.redirect('/', True)
+            return
+
+        form = CommentForm(self.request.params)
+
+        if not form.validate():
+            form.csrf_token.data = self.generate_csrf()
+            self.redirect(self.request.referer)
+            return
+
+        k = form.key.data
+        post = Key(urlsafe=k).get()
+        if post is None:
+            self.redirect(self.request.referer)
+            return
+
+        try:
+            comment = Comment(
+                parent=post.key,
+                author=user,
+                content=form.comment.data
+            )
+            comment.put()
+            self.redirect(self.request.referer)
+            return
+        except Exception as e:
+            print e.message
+            self.redirect(self.request.referer)
+            return
