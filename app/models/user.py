63f2ebf30aedfaf1427b04dfcf9ce9527a286bda
Jon Welker <JWelker110@gmail.com>
2016-06-25 07:21:09 -0500

    Remove lowercase username attr

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,5 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     username = ndb.StringProperty(required=True)
-    username_lower = ndb.StringProperty(required=True)
     password = ndb.StringProperty(required=True)
     joined = ndb.DateTimeProperty(auto_now_add=True)
