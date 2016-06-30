d869115a67b971a2ede75c41b98f9d3b66fce842
Jon Welker <JWelker110@gmail.com>
2016-06-30 05:41:19 -0500

    Add like button funcitonality on the post

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,13 @@
+(function(){
+    "use strict";
 
+    var likeBtn = document.getElementById('like-btn');
+    var likeForm = document.getElementById('like-form');
+    if (!likeBtn) { return; }
+    likeBtn.addEventListener('click', function(e){
+        if (likeForm) {
+            likeForm.submit();
+        }
+    });
+
+})();
