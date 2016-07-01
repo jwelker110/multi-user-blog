f3a186852540ae76d28dd7e1c708d396e0cfb50c
Jon Welker <JWelker110@gmail.com>
2016-07-01 04:46:33 -0500

    Increase size of shortened text

--- app/helpers/filters.py
+++ app/helpers/filters.py
@@ -5,5 +5,5 @@ def datetimefilter(value, f='%b %d, %Y'):
     return datetime.strftime(value, f)
 
 
-def shortenfilter(value, size=97):
-    return value if len(value) < 48 else value[:size] + '...'
+def shortenfilter(value, size=247):
+    return value[:size] + '...'
