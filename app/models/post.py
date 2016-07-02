e03024574fb800aa70b2f5bb48c8a6de95c3dd38
Jon Welker <JWelker110@gmail.com>
2016-07-02 08:27:04 -0500

    Remove lower case title

--- app/models/post.py
+++ app/models/post.py
@@ -3,7 +3,6 @@ from google.appengine.ext import ndb
 
 class Post(ndb.Model):
     title = ndb.StringProperty(required=True)
-    title_lower = ndb.StringProperty(required=True)
     author = ndb.StringProperty(required=True)
     author_lower = ndb.StringProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
